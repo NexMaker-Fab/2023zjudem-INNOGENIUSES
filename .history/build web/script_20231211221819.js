758a47c57d0a8e9febe59f98322e46796a0e3583
Ashraful756 <106229197+Ashraful756@users.noreply.github.com>
2023-12-12 22:09:44 +0800

    Revert "Merge branch 'main' of https://github.com/NexMaker-Fab/2023zjudem-INNOGENIUSES"
    This reverts commit 2d95f094627333778216322e9e544817ac9dae6a, reversing
    changes made to 7fed6fbec1a8491ed9c4234f98845185426de93d.

--- .history/build web/script_20231211221819.js
+++
@@ -1,8 +0,0 @@
-// Code Box JS Code
-let codeBox = document.getElementById("codeBox");
-let codeBox = document.getElementById("codeButton");
-codeButton.onclick = function() {
-    codeBox.select();
-    document.execCommand("copy");
-    codeButton.innerText = "Codes Copied";
-};
--- .history/build web/script_20231211221927.js
+++
@@ -1,8 +0,0 @@
-// Code Box JS Code
-let codeBox = document.getElementById("codeBox");
-let codeButton = document.getElementById("codeButton");
-codeButton.onclick = function() {
-    codeBox.select();
-    document.execCommand("copy");
-    codeButton.innerText = "Codes Copied";
-};
